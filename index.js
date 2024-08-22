6ccbb4edd092ecb5e0b4fde719fd27c492d2a3eb
Ronnie Aristil <ronnie.aristil@gmail.com>
2024-08-22 13:25:47 -0700

    Completed DOM Manipulation Lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// Remove the <main> element
+const main = document.getElementById("main");
+main.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Assign the id 'victory' to the newHeader element
+newHeader.id = "victory";
+
+// Set the text content of the newHeader element
+newHeader.textContent = "Ronnie Aristil is the champion";
+
+// Append the newHeader element to the body
+document.body.append(newHeader);
+
